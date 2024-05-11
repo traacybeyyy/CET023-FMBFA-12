5bcd0d70afbed2cdf19bfa734a91d26f73602881
traacybeyyy <83574620+traacybeyyy@users.noreply.github.com>
2024-05-11 14:58:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,14 @@ def main():
     r = request.form.get("q")  ## receive what front end post to backend. random convention: frontend = q, backend = r. refer to PyPi to find out the naming convention
     return(render_template("main.html", r=r)) ## right r = r above (backend), left r is the frontend r  
 
+@app.route("/prediction", methods=["GET","POST"])   
+def prediction(): 
+    return(render_template("prediction.html")) ## right r = r above (backend), left r is the frontend r  
+
+@app.route("/dbs_price", methods=["GET","POST"])   
+def dbs_price(): 
+    q = float(request.form.get("q"))  ## receive what front end post to backend. random convention: frontend = q, backend = r. refer to PyPi to find out the naming convention
+    return(render_template("dbs_price.html", r=(q*-50.6)+90.2)) ## right r = r above (backend), left r is the frontend r  
+
 if __name__ == "__main__":  
     app.run()
